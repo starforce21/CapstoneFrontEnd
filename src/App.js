d546f2a562d5b1ccf23e38392b3ff6d9bd6741d8
starforce21 <jackjack1214@gmail.com>
2022-01-31 17:25:54 -0500

    update

--- src/App.js
+++ src/App.js
@@ -23,7 +23,6 @@ function App() {
           </MarketState>
           <Nav />
           <Routes>
-            <Route path="/Mod2" element={<Home />} />
             <Route path="/home" element={<Home />} />
             <Route path="/amazon" element={<Amazon />} />
             <Route path="/apple" element={<Apple />} />
